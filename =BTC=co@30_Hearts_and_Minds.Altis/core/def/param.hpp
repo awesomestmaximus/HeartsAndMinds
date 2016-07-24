8546651fb33b9ecac4e2a19ba0e03be324a43ea2
Vdauphin <vdauphin@outlook.fr>
2016-07-24 19:26:30 +0200

    Tweak : aimingskill

--- =BTC=co@30_Hearts_and_Minds.Altis/core/def/param.hpp
+++ =BTC=co@30_Hearts_and_Minds.Altis/core/def/param.hpp
@@ -123,7 +123,7 @@ class Params {
         title = "Set skill, aimingAccuracy";
 		values[]={0,1,2,3,4,5,6,7,8,9,10,20,30,40,50,60,70,80,90,100};
 		texts[]={"0","0.1","0.2","0.3","0.4","0.5","0.6","0.7","0.8","0.9","1","2","3","4","5","6","7","8","9","10"};
-		default = 4;
+		default = 3;
 	};
     class btc_p_set_skill_aimingShake {
 	//paramsArray[18]
